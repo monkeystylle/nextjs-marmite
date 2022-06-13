1a4bf5c5d053a35b7d5dbb805d6b8fe0dc4a1814
meng <monkeystylle@gmail.com>
2022-06-14 06:36:53 +0800

    added contentful client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,32 @@
 import styled from 'styled-components';
-import type { NextPage } from 'next';
-import Navbar from '../components/Navbar';
-import Head from 'next/head';
+import type { NextPage, GetStaticProps } from 'next';
+import { createClient } from 'contentful';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ recipes }: any) => {
+  console.log('RECIPES:', recipes);
+
   return (
     <>
-      <div className="recipe-list">Recipe List</div>
+      <div>Recipe List</div>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({
+    content_type: 'recipe',
+  });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
 };
 
 const PageWrapper = styled.div``;
