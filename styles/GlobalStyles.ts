f98631baf96cc4f4b76235fc41e18d319e2fa100
meng <monkeystylle@gmail.com>
2022-06-18 07:46:21 +0800

    change font source from fontsource to google font due to FOT

--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -1,10 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
-import '@fontsource/roboto/100.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import '@fontsource/roboto/900.css';
+// import '@fontsource/roboto/100.css';
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+// import '@fontsource/roboto/900.css';
 
 const GlobalStyles = createGlobalStyle`
 
